9cd832870507afce2fbf87add3e84ec11a01b529
Joenstalker <2301107552@student.buksu.edu.ph>
2026-02-21 20:17:42 +0800

    Fix syntax error in welcome.blade.php - remove stray @if/@endif block

--- sms-app/resources/views/welcome.blade.php
+++ sms-app/resources/views/welcome.blade.php
@@ -574,9 +574,5 @@
                 </div>
             </main>
         </div>
-
-        @if (Route::has('login'))
-            <div class="h-14.5 hidden lg:block"></div>
-        @endif
     </body>
 </html>
